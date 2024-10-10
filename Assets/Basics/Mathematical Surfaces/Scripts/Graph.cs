32a3e79a00de007a77434fe6e2f650d596071f76
Pedro Gomes Antunes <darkenessninjabr@hotmail.com>
2024-10-10 19:50:25 -0300

    First commit + basics graph and mathematical surfaces

---
+++ Assets/Basics/Mathematical Surfaces/Scripts/Graph.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+namespace MATHEMATICALSURFACES
+{
+    public class Graph : MonoBehaviour
+    {
+        [SerializeField]
+        Transform pointPrefab;
+
+        [SerializeField]
+        int resolution = 10;
+
+        [SerializeField]
+        FunctionLibrary.FunctionName function;
+
+        Transform[] points;
+
+        void Awake()
+        {
+            float step = 2f / resolution;
+            Vector3 scale = Vector3.one * step;
+            points = new Transform[resolution * resolution];
+            for (int i = 0; i < points.Length; i++)
+            {
+                Transform point = points[i] = Instantiate(pointPrefab);
+                point.localScale = scale;
+                point.SetParent(transform, false);
+            }
+        }
+
+        void Update()
+        {
+            FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+            float time = Time.time;
+            float step = 2f / resolution;
+            float v = 0.5f * step - 1f;
+            for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+            {
+                if(x == resolution)
+                {
+                    x = 0;
+                    z += 1;
+                    v = (z + 0.5f) * step - 1f;
+                }
+                float u = (x + 0.5f) * step - 1f;
+                points[i].localPosition = f(u,v, time);
+            }
+        }
+    }
+}
