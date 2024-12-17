d1e935e15f572417411229b1bb69874d11b20459
Pedro Gomes Antunes <darkenessninjabr@hotmail.com>
2024-12-17 19:05:01 -0300

    Simplex Noise

---
+++ Assets/Pseudorandom Noise/Simplex Noise/Scripts/Noise.Voronoi.Function.cs
@@ -0,0 +1,27 @@
+using Unity.Mathematics;
+
+namespace SIMPLEXNOISE
+{
+    public static partial class Noise
+    {
+        public interface IVoronoiFunction
+        {
+            public float4 Evaluate(float4x2 minima);
+        }
+
+        public struct F1 : IVoronoiFunction
+        {
+            public float4 Evaluate(float4x2 distances) => distances.c0;
+        }
+
+        public struct F2 : IVoronoiFunction
+        {
+            public float4 Evaluate(float4x2 distances) => distances.c1;
+        }
+
+        public struct F2MinusF1 : IVoronoiFunction
+        {
+            public float4 Evaluate(float4x2 distances) => distances.c1 - distances.c0;
+        }
+    }
+}
